5c6ceab6a3ffa614309476b18704649a41b3f582
mihail-sinicyn <sinicyn.mihail@gmail.com>
2021-11-18 19:01:05 +0300

    2. Каркас шахматной доски [#792]

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -11,16 +11,18 @@ public class Win {
         }
         return rsl;
     }
+
     public static boolean monoVertical(int[][] board, int j) {
         boolean rsl = true;
-        for (int i = 0; i < board.length; i++) {
-            if (board[i][j] != 1) {
+        for (int[] ints : board) {
+            if (ints[j] != 1) {
                 rsl = false;
                 break;
             }
         }
         return rsl;
     }
+
     public static boolean check(int[][] board) {
         boolean rsl = false;
         for (int i = 0; i < board.length; i++) {
